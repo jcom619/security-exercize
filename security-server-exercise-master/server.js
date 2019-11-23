b9d672d574e71473aa04bd598028e4f3de265740
jcom619 <46643961+jcom619@users.noreply.github.com>
2019-11-23 15:09:47 -0800

    nothing really

--- security-server-exercise-master/server.js
+++ security-server-exercise-master/server.js
@@ -2,22 +2,18 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
-var winston = require('winston');
+const winston = require('winston');
 
 const bodyParser = require('body-parser');
-const app = express()
-app.use(cors())
-app.use(helmet())
-app.use(bodyParser.json())
-app.use(morgan('tiny'))
+const app = express();
+app.use(cors());
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(morgan('tiny'));
 
 app.get('/', (req, res) => {
-  res.cookie('session', '1', {httpOnly: true})
-  res.cookie('session', '1', {secure: true})
-  res.set({
-    'Content-Security-Policy': "script-src 'self' 'https://apis.google.com'"
-  })
-  res.send('HiMundo!')
+
+  res.send('HiMundo!');
 });
 
 app.post('/secret', (req, res) => {
